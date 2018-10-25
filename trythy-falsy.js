9b29b20608d0e7ed058a3ef65a9090c89ff67090
camilo <camilomontoya2@gmail.com>
2018-10-25 11:06:12 -0500

    ejemplos truthy - falsy

---
+++ trythy-falsy.js
@@ -0,0 +1,14 @@
+console.log('suma', 1 + 1);
+console.log('suma', 1 + '1');
+console.log('suma', '1' + 1);
+
+console.log(0 == false);
+console.log(1 == true);
+console.log(1 + true);
+console.log(2 + true);
+
+console.log(!!'  ');
+
+const array1 = [3];
+
+console.log('esta lleno', !!array1.length, array1.length);
